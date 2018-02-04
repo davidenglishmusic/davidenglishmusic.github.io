44094e3754f91e4502cb10f705bf61e18a1cb549
davidenglishmusic <david@operaonfilm.com>
2018-02-04 15:16:39 -0400

    switches to less and adds a build script

---
+++ scripts/build.sh
@@ -0,0 +1,4 @@
+lessc --clean-css assets/stylesheets/index.less assets/stylesheets/index.min.css
+lessc --clean-css assets/stylesheets/reading_list.less assets/stylesheets/reading_list.min.css
+lessc --clean-css assets/stylesheets/shared.less assets/stylesheets/shared.min.css
+lessc --clean-css assets/stylesheets/side_projects.less assets/stylesheets/side_projects.min.css
