53149064ee58f87f7ca9bd05834737f4e010f29a
davidenglishmusic <david@operaonfilm.com>
2018-11-07 20:33:09 -0400

    Add updated cviceni

---
+++ cviceni/precache-manifest.b2e9de647a9a3a8f704efe540205513b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed0d57d1a1b914942d19",
+    "url": "/cviceni/static/js/runtime~main.ed0d57d1.js"
+  },
+  {
+    "revision": "c600aeaf2859134eabc3",
+    "url": "/cviceni/static/js/main.c600aeaf.chunk.js"
+  },
+  {
+    "revision": "863a4a8776cd94ba0b1f",
+    "url": "/cviceni/static/js/1.863a4a87.chunk.js"
+  },
+  {
+    "revision": "c600aeaf2859134eabc3",
+    "url": "/cviceni/static/css/main.3c7ebdbc.chunk.css"
+  },
+  {
+    "revision": "4bc42cb047c80a9b2399d26e3fa8d227",
+    "url": "/cviceni/index.html"
+  }
+];
